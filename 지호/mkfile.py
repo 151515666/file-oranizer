9f8b32e69d508d810905b3ad33bbe314501817c4
지이호오 <jiho@jihOooO.local>
2026-03-06 20:18:11 +0900

    수정

--- 지호/mkfile.py
+++ 지호/mkfile.py
@@ -4,21 +4,6 @@ import shutil
 
 def fl():
 
-
-    # 현재 디렉토리 출력
-    # print(os.getcwd())
-
-    # 현재경로에 파일 만들기
-    # os.mkdir("/Users/jiho/file-oranizer/test_mkdir")
-
-    # 현재 디렉토리 내의 모든 파일과 폴더명 리스트로 변환
-    # os.listdir("/디렉토리")
-    # print(os.listdir(os.getcwd()))
-
-    # 확장자 출력하는법
-    # result = os.path.splitext("사진1.jpg")
-    # print(result[1])
-
     folder_path = input("정리할 폴더 경로를 입력하세요:")
     if not os.path.exists(folder_path):
         print("존재하지 않는 폴더 입니다.")
@@ -41,6 +26,7 @@ def fl():
         dst_path = os.path.join(folder_path, folder_name) #이동할 목적지 폴더 경로 만들기 /User/sadasd/file/사진 이런식
 
         if os.path.isfile(full_path) and folder_name != "기타":
+            os.makedirs(dst_path, exist_ok=True)
             shutil.move(full_path, dst_path)
             print(filename, folder_name)
 fl()
