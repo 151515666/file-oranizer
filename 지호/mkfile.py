d0aaa4f962da7dbea25da903d0c2f39b2b198b2d
지이호오 <jiho@jihOooO.local>
2026-03-06 20:09:11 +0900

    주석달았음

--- 지호/mkfile.py
+++ 지호/mkfile.py
@@ -34,11 +34,11 @@ def fl():
         ".mp4": "영상",
     }
 
-    for filename in files:
-        result = os.path.splitext(filename)
-        folder_name = extension_map.get(result[1], "기타")
-        full_path = os.path.join(folder_path, filename)
-        dst_path = os.path.join(folder_path, folder_name)
+    for filename in files:   #files리스트에서 파일 이름을 하나씩 꺼내 filename에 담기
+        result = os.path.splitext(filename)  #파일 이름을 이름이랑 확장자로 쪼개기 예)사진1.jpg면 ('사진1', '.jpg')
+        folder_name = extension_map.get(result[1], "기타") #result[1]로 확장자만 꺼내서 딕셔너리에 찾기 .jpg면 "사진" 없으면 "기타"
+        full_path = os.path.join(folder_path, filename) #파일 전체 경로 만들기
+        dst_path = os.path.join(folder_path, folder_name) #이동할 목적지 폴더 경로 만들기 /User/sadasd/file/사진 이런식
 
         if os.path.isfile(full_path) and folder_name != "기타":
             shutil.move(full_path, dst_path)
